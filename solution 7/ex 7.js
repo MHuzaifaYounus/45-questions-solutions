da1ab1304aef74c31842547bea654b65fcb71af1
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-20 23:28:31 +0500

    uploading ex 7

---
+++ solution 7/ex 7.js
@@ -0,0 +1,11 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+var num1 = 6;
+var num2 = 2;
+var num3 = 10;
+var num4 = 4;
+var num5 = 16;
+var add = (num1 + num2);
+var sub = (num3 - num2);
+var mult = (num4 * num2);
+var divi = (num5 / num2);
+console.log("Addition : ".concat(add, " \nSubtraction : ").concat(sub, "\nMultiplication : ").concat(mult, "\nDivision : ").concat(divi));
---
+++ solution 7/ex 7.ts
@@ -0,0 +1,14 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let num1 = 6;
+let num2 = 2;
+let num3 = 10;
+let num4 = 4;
+let num5 = 16;
+let add = (num1 + num2);
+let sub = (num3 - num2);
+let mult = (num4 * num2);
+let divi = (num5 / num2);
+console.log(`Addition : ${add} 
+Subtraction : ${sub}
+Multiplication : ${mult}
+Division : ${divi}`);
