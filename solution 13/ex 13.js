87aa59698d3c1ed53cac89df244da1e84406a338
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-21 00:09:01 +0500

    uploading ex 13

---
+++ solution 13/ex 13.js
@@ -0,0 +1,7 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var list = ["Bicycle", "Motorcycle", "Helicopter", "Train", "Red Bus"];
+console.log("".concat(list[0], " : A bicycle is a useful vehicle that helps us reach a destination without polluting the environment."));
+console.log("".concat(list[1], " : motorcycle accidents had increasingly become a problem for countries "));
+console.log("".concat(list[2], " : I have always admired the power and grace of a sleek Bell 407 helicopter."));
+console.log("".concat(list[3], " : Some trains are old and make a chugging sound, which is nice."));
+console.log("".concat(list[4], " : The red buses in Pakistan are a common sight on the roads."));
---
+++ solution 13/ex 13.ts
@@ -0,0 +1,8 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let list = ["Bicycle","Motorcycle","Helicopter","Train","Red Bus"];
+
+console.log(`${list[0]} : A bicycle is a useful vehicle that helps us reach a destination without polluting the environment.`)
+console.log(`${list[1]} : motorcycle accidents had increasingly become a problem for countries `)
+console.log(`${list[2]} : I have always admired the power and grace of a sleek Bell 407 helicopter.`)
+console.log(`${list[3]} : Some trains are old and make a chugging sound, which is nice.`)
+console.log(`${list[4]} : The red buses in Pakistan are a common sight on the roads.`)
