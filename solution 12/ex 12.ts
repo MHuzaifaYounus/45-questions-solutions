dfee720f84149be6e1f04a614fb802889ae7fbb3
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-20 23:57:40 +0500

    uploading ex 12

---
+++ solution 12/ex 12.ts
@@ -0,0 +1,5 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var names = ["Adil", "Areeb", "Anus", "Irtiza"];
+for (var key = 0; key < names.length; key++) {
+    console.log("Good to see you : "+ names[key]);
+}
