880ac78a82ff796e57296ad40b4e63cd417772ed
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-21 02:01:22 +0500

    uploading ex 20

---
+++ solution 20/ex 20.js
@@ -0,0 +1,13 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var a = "Shikayat";
+var b = "Shor";
+var c = "Iraaday";
+var d = "Samjho na";
+var e = "Bhikhra";
+var songs = ["songs"];
+songs.push(a);
+songs.push(b);
+songs.push(c);
+songs.push(d);
+songs.push(e);
+console.log(songs);
---
+++ solution 20/ex 20.ts
@@ -0,0 +1,16 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let a = "Shikayat"
+let b = "Shor"
+let c = "Iraaday"
+let d = "Samjho na"
+let e = "Bhikhra"
+
+let songs = ["songs"]
+
+songs.push(a);
+songs.push(b);
+songs.push(c);
+songs.push(d);
+songs.push(e);
+
+console.log(songs)
