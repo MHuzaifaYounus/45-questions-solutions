f136c529dcb0a06525da6d0ba33c753b02c88432
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-20 23:51:28 +0500

    uploading ex 11

---
+++ solution 11/ex 11.ts
@@ -0,0 +1,8 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+var names =  ["Adil", "Areeb" , "Anus" , "Irtiza"];
+
+for (let key = 0; key < names.length; key++) {
+    console.log(names[key]);
+    
+}
