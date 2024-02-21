c869bba22f41fa30fbcc7fcf55024e6485b9429d
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-22 00:26:42 +0500

    uploading ex 23

---
+++ solution 23/ex 23.js
@@ -0,0 +1,36 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+var PUBG = 'Shooting Game';
+var FreeFire = 'Shooting Game';
+var Minecraft = 'Crafting';
+var Gta = 'Open World';
+var AssasinsCreed = 'Action Game';
+var Mario = '2D Game';
+var Asphalt = 'Racing Game';
+var SubwaySurfers = 'Mobile Game';
+var Tekken3 = 'Fightning Game';
+var Fortnight = 'Battelground Game';
+console.log("Is PUBG = 'Shooting Game'? I predict True.");
+console.log(PUBG == 'Shooting Game');
+console.log("Is Minecraft = 'Crafting'? I predict True.");
+console.log(Minecraft == 'Crafting');
+console.log("Is PUBG = 'Shooting Game'? I predict True.");
+console.log(Gta == 'Open World');
+console.log("Is AssasinsCreed = 'Action Game'? I predict True.");
+console.log(AssasinsCreed == 'Action Game');
+console.log("Is Mario = '2D Game'? I predict True.");
+console.log(Mario == '2D Game');
+console.log("Is Asphalt = 'Running Game'? I predict FAlse.");
+console.log(Asphalt == 'Running Game');
+console.log("Is FreeFire = 'PC Game'? I predict False.");
+console.log(FreeFire == 'PC Game');
+console.log("Is SubwaySurfers = 'PC Game'? I predict False.");
+console.log(SubwaySurfers == 'PC Game');
+console.log("Is Tekken3 = 'Open World Game'? I predict False.");
+console.log(Tekken3 == 'Open World Game');
+console.log("Is Fortnight= 'Mobile Game'? I predict False.");
+console.log(Fortnight == 'Mobile Game');
---
+++ solution 23/ex 23.ts
@@ -0,0 +1,55 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let PUBG = 'Shooting Game';
+let FreeFire = 'Shooting Game';
+let Minecraft = 'Crafting';
+let Gta = 'Open World';
+let AssasinsCreed = 'Action Game';
+let Mario = '2D Game';
+let Asphalt= 'Racing Game';
+let SubwaySurfers = 'Mobile Game';
+let Tekken3 = 'Fightning Game';
+let Fortnight= 'Battelground Game';
+
+
+console.log("Is PUBG = 'Shooting Game'? I predict True.")
+console.log(PUBG == 'Shooting Game')
+
+
+console.log("Is Minecraft = 'Crafting'? I predict True.")
+console.log(Minecraft == 'Crafting')
+
+console.log("Is PUBG = 'Shooting Game'? I predict True.")
+console.log(Gta == 'Open World')
+
+console.log("Is AssasinsCreed = 'Action Game'? I predict True.")
+console.log(AssasinsCreed == 'Action Game')
+
+
+
+console.log("Is Mario = '2D Game'? I predict True.")
+console.log(Mario == '2D Game')
+
+console.log("Is Asphalt = 'Running Game'? I predict FAlse.")
+console.log(Asphalt== 'Running Game')
+
+console.log("Is FreeFire = 'PC Game'? I predict False.")
+console.log(FreeFire == 'PC Game')
+
+console.log("Is SubwaySurfers = 'PC Game'? I predict False.")
+console.log(SubwaySurfers == 'PC Game')
+
+console.log("Is Tekken3 = 'Open World Game'? I predict False.")
+console.log(Tekken3 == 'Open World Game')
+
+console.log("Is Fortnight= 'Mobile Game'? I predict False.")
+console.log(Fortnight== 'Mobile Game')
