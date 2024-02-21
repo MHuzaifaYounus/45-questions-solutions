78be57b5b2bb826f567dcd047e76fe9cb62ec78e
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-21 23:50:23 +0500

    uploading ex 21 **

---
+++ solution 21/ex 21.js
@@ -0,0 +1,7 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var myobj = {};
+myobj.create = "Huzaifa";
+myobj.name2 = "Amna";
+myobj.name3 = "Hamza";
+myobj.name4 = "Ali";
+console.log(myobj);
