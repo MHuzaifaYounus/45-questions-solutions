0d371a75e9e013b0fd3ef3dc3b947e2e30fb1bf9
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-02-23 00:21:16 +0500

    uploading ex 40

---
+++ solution 40/ex40.ts
@@ -0,0 +1,17 @@
+// Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. 
+// Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. 
+// Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
+function make_album(artistname,albumtitle,tracks = 0) {
+    let obj = {
+        "ArtistName" : artistname,
+        "AlbumTitle" : albumtitle,
+    }
+    if(tracks > 0){
+        obj["Tracks"] = tracks
+    }
+    return obj
+}
+console.log(make_album("Jordon Sidhu","Mind Map"))
+console.log(make_album("Talha Anjum","Open letter",16))
+console.log(make_album("Taimour Beg","Azal"))
+console.log(make_album("Aditya Rikhari","Samjho na"))
